3d1f0cf0ad15e9982cf393c582a2c3518a252251
Kamal Thapaliya <kamalthapaliya7@gmail.com>
2023-01-10 15:59:03 +0545

    kkk

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Nav } from "./components/Nav";
+import Hero from "./components/Hero";
+import Reviews from "./components/Reviews";
+import { Book } from "./components/Book";
+import { Profile } from "./components/Profile";
+import { Signup } from "./components/Signup";
+import Admin from "./pages/Admin";
+import { Routes, Route , Navigate } from "react-router-dom";
+
+function App() {
+  let loggedIn = false;
+  
+  return (
+    <div className="App">
+      <Nav />
+      <Hero />
+      <Book />
+      <Profile />
+      <Reviews/>
+      <Signup />
+
+      {/* <Routes>
+        <Route path="/" element={<Admin />} >
+          <Route path="/" element={<Signup />} />
+          <Route path="/dashboard" element={<Signup />} />
+
+          <Route path="/another" element={<Profile />} />
+          <Route path="*" element={<Navigate to={'/'}/> } />
+        </Route>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
